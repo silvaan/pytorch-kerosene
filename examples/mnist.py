550b11906ed48d61f980cc6bce3abef4fa177ee5
Silvan <sjrmiranda@gmail.com>
2022-11-10 23:03:21 -0300

    add tensorboard and logging

--- examples/mnist.py
+++ examples/mnist.py
@@ -17,6 +17,11 @@ val_len = int(len(dataset)*validation_split)
 train_len = len(dataset) - val_len
 train_set, val_set = random_split(dataset, [train_len, val_len])
 
+data_loaders = {
+    'train': DataLoader(train_set, batch_size=32, shuffle=True),
+    'val': DataLoader(val_set, batch_size=128, shuffle=True)
+}
+
 
 class MNISTClassifier(nn.Module):
     def __init__(self, input_dim, n_classes):
@@ -37,9 +42,6 @@ class MyTrainer(Trainer):
     def before_train(self):
         print(f'Starting training with {self.epochs} epochs in device {self.device}...')
 
-    def after_epoch(self, epoch, metrics):
-        print(f'val loss: {metrics["val"]["loss"]}, val acc: {metrics["val"]["acc"]}')
-
     def training_step(self, batch):
         X, y = batch
         pred = self.model(X)
@@ -58,18 +60,19 @@ class MyTrainer(Trainer):
 
 
 model = MNISTClassifier(784, 10)
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
 trainer = MyTrainer(
     run_name='model_test',
     model=model,
-    criterion=nn.CrossEntropyLoss(),
-    optimizer=torch.optim.Adam(model.parameters(), lr=0.001),
+    criterion=criterion,
+    optimizer=optimizer,
     epochs=3,
-    data_loaders={
-        'train': DataLoader(train_set, batch_size=32, shuffle=True),
-        'val': DataLoader(val_set, batch_size=128, shuffle=True)
-    },
-    save_checkpoints='all'
+    data_loaders=data_loaders,
+    save_checkpoints='last',
+    checkpoints_dir='checkpoints',
+    tensorboard_dir='runs'
 )
 
 metrics = trainer.train()
--- pytorch_kerosene/trainer.py
+++ pytorch_kerosene/trainer.py
@@ -1,34 +1,73 @@
 import os
+import sys
+import logging
 from collections import defaultdict
+from datetime import datetime
 import torch
+from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
 
 
 class Trainer:
     def __init__(
         self,
-        run_name,
         model,
         criterion,
         optimizer,
         epochs,
         data_loaders,
+        model_name=None,
+        run_name=None,
         device=None,
+        save_checkpoints='last',
         checkpoints_dir='checkpoints',
-        save_checkpoints='last'
+        use_logging=True,
+        logging_dir='.',
+        use_tensorboard=True,
+        tensorboard_dir='runs'
     ):
         if device is None:
             device = torch.device('cuda:0') if torch.cuda.is_available() else 'cpu'
 
-        self.run_name = run_name
+        self.model_name = model_name or model.__class__.__name__
+        self.run_name = run_name or self.model_name+'_'+datetime.now().strftime('%b-%d_%H-%M')
+
+        # Training
         self.model = model.to(device)
         self.criterion = criterion
         self.optimizer = optimizer
         self.epochs = epochs
         self.data_loaders = data_loaders
         self.device = device
-        self.checkpoints_dir = checkpoints_dir
+
+        # Checkpoints
         self.save_checkpoints = save_checkpoints
+        self.checkpoints_dir = checkpoints_dir
+
+        # Logging
+        self.set_logger(logging_dir, run_name)
+        self.logger.disabled = not use_logging
+
+        # Tensorbaord
+        self.use_tensorboard = use_tensorboard
+        self.tensorboard_dir = tensorboard_dir
+        if self.use_tensorboard:
+            tb_logdir = os.path.join(self.tensorboard_dir, self.run_name)
+            self.tb_writer = SummaryWriter(tb_logdir)
+
+    def set_logger(self, logging_dir, run_name):
+        self.logger = logging.getLogger()
+        self.logger.setLevel(logging.DEBUG)
+
+        sh = logging.StreamHandler(sys.stdout)
+        sh.setFormatter(logging.Formatter('%(message)s'))
+        self.logger.addHandler(sh)
+
+        if logging_dir is not None:
+            os.makedirs(logging_dir, exist_ok=True)
+            fh = logging.FileHandler(os.path.join(logging_dir, run_name + '.log'))
+            fh.setFormatter(logging.Formatter('%(asctime)s: %(message)s'))
+            self.logger.addHandler(fh)
 
     # HOOKS
 
@@ -51,8 +90,10 @@ class Trainer:
 
         if self.save_checkpoints == 'all':
             filename = f'{self.run_name}_{epoch}.pt'
-        else:
+        elif self.save_checkpoints == 'last':
             filename = f'{self.run_name}.pt'
+        else:
+            filename = f'{self.run_name}_{epoch}.pt'
         
         checkpoint_filepath = os.path.join(self.checkpoints_dir, filename)
         model_dict = {
@@ -86,6 +127,14 @@ class Trainer:
             return res
         return x
 
+    def dict2log(self, metrics):
+        values = []
+        for phase in metrics.keys():
+            for metric in metrics[phase].keys():
+                values.append(f'{phase}_{metric}: {metrics[phase][metric]:.5f}')
+        output = ', '.join(values)
+        return output
+
     def training_step(self, batch):
         raise NotImplementedError
 
@@ -94,6 +143,7 @@ class Trainer:
 
     def train(self):
         self.before_train()
+        self.logger.info(f'Starting run {self.run_name}: {self.model_name} with {self.epochs} epochs in {self.device}...')
         phases = self.data_loaders.keys()
         training_metrics = {phase: defaultdict(list) for phase in phases}
 
@@ -120,21 +170,39 @@ class Trainer:
                 # Validation phase
                 else:
                     self.model.eval()
-                    for batch in self.data_loaders[phase]:
-                        batch = self.to_device(batch)
-                        with torch.no_grad():
-                            metrics = self.validation_step(batch)
+                    with torch.no_grad():
+                        for batch in self.data_loaders[phase]:
+                            batch = self.to_device(batch)
+                            with torch.no_grad():
+                                metrics = self.validation_step(batch)
 
-                        for metric, value in metrics.items():
-                            temp_metrics[metric].append(value.detach().item())
+                            for metric, value in metrics.items():
+                                temp_metrics[metric].append(value.detach().item())
                             
                 # Average metrics across batches
                 for metric, values in temp_metrics.items():
                     epoch_metrics[phase][metric] = sum(values)/len(values)
-                    training_metrics[phase][metric].append(sum(values)/len(values))
+                    training_metrics[phase][metric].append(epoch_metrics[phase][metric])
 
-            self.save_checkpoint(epoch)
+                # Log metrics in Tensorboard
+                if self.use_tensorboard:
+                    for metric in epoch_metrics[phase]:
+                        self.tb_writer.add_scalar(f'{phase}/{metric}', epoch_metrics[phase][metric], epoch)
+                    
+                    for name, weight in self.model.named_parameters():
+                        self.tb_writer.add_histogram(name, weight, epoch)
+
+            if self.save_checkpoints is not None:
+                self.save_checkpoint(epoch)
+            
+            self.logger.info(self.dict2log(epoch_metrics))
+            
             self.after_epoch(epoch=epoch, metrics=epoch_metrics)
+
+        if self.use_tensorboard:
+            self.tb_writer.close()
+
+        self.logger.info('Finished training.')
 
         training_metrics = {k: dict(v) for k, v in training_metrics.items()}
         self.after_train(metrics=training_metrics)
